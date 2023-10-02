26df725838e0f01f17d810232791503b499de519
Solonas Astraios <solonas.astraios@barclays.com>
2023-10-02 13:13:19 +0100

    first scene files and controllers

---
+++ src/main/java/controller/appStartController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class appStartController {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    public void handleLoginButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/controller/loginController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class loginController {
+    @javafx.fxml.FXML
+    private Label usenameLabel;
+    @javafx.fxml.FXML
+    private Label passwordLabel;
+    @javafx.fxml.FXML
+    private TextField userNameTextField;
+    @javafx.fxml.FXML
+    private TextField passwordTextField;
+    @javafx.fxml.FXML
+    private Button loginButton;
+
+    @javafx.fxml.FXML
+    public void handleLoginButton(ActionEvent actionEvent) {
+    }
+}
