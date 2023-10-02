8dd2d3cac2fde0c57b11616201cc0e3aaa011e93
Solonas Astraios <solonas.astraios@barclays.com>
2023-10-02 14:46:17 +0100

    Added Back Buttons, created dashboard scene, updated signup controller function

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,9 +1,17 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class LoginController {
     @javafx.fxml.FXML
@@ -16,8 +24,22 @@ public class LoginController {
     private TextField passwordTextField;
     @javafx.fxml.FXML
     private Button loginButton;
+    @javafx.fxml.FXML
+    private Button backButton;
 
     @javafx.fxml.FXML
     public void handleLoginButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBackButton(@NotNull ActionEvent actionEvent) throws IOException {
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(AppStartController.class.getResource("/capstonebankapp/app-start-scene.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+        currentStage.close();
+        newStage.show();
     }
 }
---
+++ src/main/java/controller/SignUpController.java
@@ -0,0 +1,87 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignUpController {
+    @javafx.fxml.FXML
+    private Label newUserNameLabel;
+    @javafx.fxml.FXML
+    private Label enterPasswordLabel;
+    @javafx.fxml.FXML
+    private Label reEnterPasswordLabel;
+    @javafx.fxml.FXML
+    private TextField newUserNameTextFiend;
+    @javafx.fxml.FXML
+    private TextField enterPasswordTextField;
+    @javafx.fxml.FXML
+    private TextField reEnterPasswordTextField;
+    @javafx.fxml.FXML
+    private Button createAccountButton;
+    @javafx.fxml.FXML
+    private Button backButton;
+    @javafx.fxml.FXML
+    private Label createAccountErrorMessage;
+
+    private static boolean checkString(String password) {
+        char ch;
+        boolean capitalFlag = false;
+        boolean lowerCaseFlag = false;
+        boolean numberFlag = false;
+        for(int i=0;i < password.length();i++) {
+            ch = password.charAt(i);
+            if( Character.isDigit(ch)) {
+                numberFlag = true;
+            }
+            else if (Character.isUpperCase(ch)) {
+                capitalFlag = true;
+            } else if (Character.isLowerCase(ch)) {
+                lowerCaseFlag = true;
+            }
+            if(numberFlag && capitalFlag && lowerCaseFlag)
+                return true;
+        }return false;
+    }
+
+    @javafx.fxml.FXML
+    public void handleCreateAccountButton(ActionEvent actionEvent) throws IOException {
+        String username = newUserNameTextFiend.getText();
+        String password = enterPasswordTextField.getText();
+        String passwordCheck = reEnterPasswordTextField.getText();
+        if (password.equals(passwordCheck)) {
+           boolean passwordConditions = checkString(password);
+           if (passwordConditions){
+               Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+               FXMLLoader fxmlLoader = new FXMLLoader(AppStartController.class.getResource("/capstonebankapp/login-scene.fxml"));
+               Parent root = fxmlLoader.load();
+               Stage newStage = new Stage();
+               newStage.setScene(new Scene(root));
+               currentStage.close();
+               newStage.show();
+           }else createAccountErrorMessage.setText("Password conditions not met!");
+            }else createAccountErrorMessage.setText("Passwords do not match!");
+        }
+
+
+    @javafx.fxml.FXML
+    public void handleBackButton(ActionEvent actionEvent) throws IOException {
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(AppStartController.class.getResource("/capstonebankapp/app-start-scene.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+        currentStage.close();
+        newStage.show();
+    }
+}
