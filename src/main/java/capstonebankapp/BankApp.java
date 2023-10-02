9fcbd8baa8a4293c1c019dbf7beb3a186b686ef8
Max Fawcett <maxfawcett01@gmail.com>
2023-10-02 13:48:41 +0100

    Added Button function and code to make the app run in BankApp.java

--- src/main/java/capstonebankapp/BankApp.java
+++ src/main/java/capstonebankapp/BankApp.java
@@ -1,13 +1,23 @@
 package capstonebankapp;
 
+import controller.AppStartController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class BankApp extends Application {
 
     @Override
-    public void start(Stage stage) {
-
+    public void start(@NotNull Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AppStartController.class.getResource("/capstonebankapp/app-start-scene.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/capstonebankmodel/Bank.java
+++ src/main/java/capstonebankmodel/Bank.java
@@ -1,4 +1,53 @@
 package capstonebankmodel;
 
 public class Bank implements IBank{
+    @Override
+    public void withdraw(Account account, double amount) {
+
+    }
+
+    @Override
+    public void deposit(Account account, double amount) {
+
+    }
+
+    @Override
+    public void transfer(Account sender, Account recipient, double amount) {
+
+    }
+
+    @Override
+    public void addAccount(Customer customer, Account account) {
+
+    }
+
+    @Override
+    public void deleteAccount(String accountId) {
+
+    }
+
+    @Override
+    public void initializeAccounts() {
+
+    }
+
+    @Override
+    public void takeLoan(Account account, Loan loan) {
+
+    }
+
+    @Override
+    public void addCustomer(String firstName, String lastName, String username, String password) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String customerId) {
+
+    }
+
+    @Override
+    public void getAccountMap() {
+
+    }
 }
