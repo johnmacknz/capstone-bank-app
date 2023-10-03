c74d6485656f8fc555dbc6b44f6938e4fcfc76a3
Max Fawcett <maxfawcett01@gmail.com>
2023-10-03 16:34:59 +0100

    First work done on Dashboard

---
+++ src/main/java/capstonebankapp/SceneTest.java
@@ -0,0 +1,27 @@
+package capstonebankapp;
+
+import controller.AppStartController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+import java.io.IOException;
+
+
+
+public class SceneTest extends Application{
+    @Override
+    public void start(@NotNull Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AppStartController.class.getResource("/capstonebankapp/dashboard-scene.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
