66b344ea7add094e9cb09973bb3f5156e040dc18
Max Fawcett <maxfawcett01@gmail.com>
2023-10-04 12:11:40 +0100

    Updated Dashboard, created deposit scene and controller

--- src/main/java/controller/CreateNewAccountController.java
+++ src/main/java/controller/CreateNewAccountController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.URL;
@@ -39,7 +40,7 @@ public class CreateNewAccountController implements Initializable {
     }
 
     @javafx.fxml.FXML
-    public void handleBackButton(ActionEvent actionEvent) throws IOException {
+    public void handleBackButton(@NotNull ActionEvent actionEvent) throws IOException {
         Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
         FXMLLoader fxmlLoader = new FXMLLoader(AppStartController.class.getResource("/capstonebankapp/dashboard-scene.fxml"));
