946f8f69b80058fa5a507eb171e90fe440c5a782
John <jmmac2000@gmail.com>
2023-11-02 15:00:31 +0000

    refactoring

--- src/main/java/capstonebankmodel/Bank.java
+++ src/main/java/capstonebankmodel/Bank.java
@@ -2,14 +2,8 @@ package capstonebankmodel;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-import org.apache.commons.csv.CSVRecord;
 
 public class Bank {
 
@@ -49,41 +43,27 @@ public class Bank {
 
     public void withdraw(Account account, double amount) {
         account.withdraw(amount);
-        csvEditAccountBalance(account.getAccountId(), account.getBalance());
+        CsvManager.csvEditAccountBalance(account.getAccountId(), account.getBalance());
     }
 
 
     public void deposit(Account account, double amount) {
         account.deposit(amount);
-        csvEditAccountBalance(account.getAccountId(), account.getBalance());
+        CsvManager.csvEditAccountBalance(account.getAccountId(), account.getBalance());
     }
 
     public void transfer(Account sender, Account recipient, double amount) {
         sender.transferTo(amount, recipient);
-        csvEditAccountBalance(sender.getAccountId(), sender.getBalance());
-        csvEditAccountBalance(recipient.getAccountId(), recipient.getBalance());
+        CsvManager.csvEditAccountBalance(sender.getAccountId(), sender.getBalance());
+        CsvManager.csvEditAccountBalance(recipient.getAccountId(), recipient.getBalance());
     }
 
     public void addAccount(Customer customer, String accountType) {
         // NEW ACCOUNT
         Account account = AccountFactory.generateAccount(accountType);
         accountDataHashMap.put(account.getAccountId(), account);
-        csvAddAccountRecord(account, customer.getUserName());
+        CsvManager.csvAddAccountRecord(account, customer.getUserName());
         customer.addAccount(account);
-    }
-
-    private void csvAddAccountRecord(Account account, String customerName) {
-        String csvFilePath = "src/main/resources/data/account-data.csv";
-        String[] recordToAdd = {String.valueOf(account.getAccountId()), customerName,
-                account.ACCOUNT_TYPE, String.valueOf(account.getBalance())};
-        CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader();
-        try (Writer fileWriter = new FileWriter(csvFilePath, true);
-             CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT)) {
-            csvPrinter.printRecord((Object[]) recordToAdd);
-            csvPrinter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void addAccount(Customer customer, String accountType, long accountId, double balance) {
@@ -91,11 +71,6 @@ public class Bank {
         Account account = AccountFactory.generateAccount(accountType, accountId, balance);
         accountDataHashMap.put(account.getAccountId(), account);
         customer.addAccount(account);
-    }
-
-    public void deleteAccount(long accountId) {
-        accountDataHashMap.remove(accountId);
-        csvDeleteAccount(accountId);
     }
 
     public void initializeHashMaps() {
@@ -147,7 +122,7 @@ public class Bank {
     public void addNewLoan(Customer customer, String loanType, double loanAmount, int loanDuration) {
         Loan loan = LoanFactory.generateLoan(loanType, loanAmount, loanDuration);
         loanDataHashMap.put(loan.getLoanId(), loan);
-        csvAddLoan(loan, customer.getUserName());
+        CsvManager.csvAddLoan(loan, customer.getUserName());
         customer.addLoan(loan);
     }
 
@@ -158,26 +133,11 @@ public class Bank {
         customer.addLoan(loan);
     }
 
-    private void csvAddLoan(Loan loan, String username) {
-        String csvFilePath = "src/main/resources/data/loan-data.csv";
-        String[] recordToAdd = {String.valueOf(loan.getLoanId()), username, loan.loanType,
-                String.valueOf(loan.getLoanAmount()), String.valueOf(loan.getOutstandingAmount()),
-                String.valueOf(loan.getLoanDuration()), String.valueOf(loan.getLoanDate())};
-        CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader();
-        try (Writer fileWriter = new FileWriter(csvFilePath, true);
-             CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT)) {
-            csvPrinter.printRecord((Object[]) recordToAdd);
-            csvPrinter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public void addNewCustomer(String username, String firstName, String lastName, String password) {
         // NEW CUSTOMER
         Customer customer = new Customer(username, firstName, lastName, password);
         customerDataHashMap.put(customer.getUserName(), customer);
-        csvAddCustomerRecord(customer);
+        CsvManager.csvAddCustomerRecord(customer);
     }
 
     public void addCustomer(String username, String firstName, String lastName, String password) {
@@ -186,166 +146,22 @@ public class Bank {
         customerDataHashMap.put(customer.getUserName(), customer);
     }
 
-    private void csvAddCustomerRecord(Customer customer) {
-        String csvFilePath = "src/main/resources/data/customer-data.csv";
-        String[] recordToAdd = {customer.getUserName(), customer.getFirstName(), customer.getLastName(), customer.getPassword()};
-        CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader();
-        try (Writer fileWriter = new FileWriter(csvFilePath, true);
-             CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT)) {
-            csvPrinter.printRecord((Object[]) recordToAdd);
-            csvPrinter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void csvDeleteAccount(long accountNumber) {
-        String tempFilePath = "src/main/resources/data/temporary-file.csv";
-        File accountData = new File("src/main/resources/data/account-data.csv");
-        File tempFile = new File(tempFilePath);
-        try {
-            FileWriter fw = new FileWriter(tempFilePath, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            Scanner fileScanner = new Scanner(new File("src/main/resources/data/account-data.csv"));
-            if (fileScanner.hasNextLine()) {
-                String headerLine = fileScanner.nextLine();
-                pw.println(headerLine);
-            }
-            while(fileScanner.hasNext()) {
-                String accountInfo = fileScanner.nextLine();
-                String[] accountInfoArray = accountInfo.split(",");
-                if (Long.parseLong(accountInfoArray[0]) != accountNumber) {
-                    pw.print(accountInfoArray[0] + "," + accountInfoArray[1] + "," + accountInfoArray[2] + "," + accountInfoArray[3]);
-                }
-            }
-            fileScanner.close();
-            pw.flush();
-            pw.close();
-            accountData.delete();
-            File dump = new File("src/main/resources/data/account-data.csv");
-            tempFile.renameTo(dump);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void csvEditAccountBalance(long accountNumber, double newBalance) {
-        String tempFilePath = "src/main/resources/data/temporary-file.csv";
-        File accountData = new File("src/main/resources/data/account-data.csv");
-        File tempFile = new File(tempFilePath);
-        try {
-            FileWriter fw = new FileWriter(tempFilePath, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            Scanner fileScanner = new Scanner(new File("src/main/resources/data/account-data.csv"));
-            if (fileScanner.hasNextLine()) {
-                String headerLine = fileScanner.nextLine();
-                pw.println(headerLine);
-            }
-            while(fileScanner.hasNext()) {
-                 String accountInfo = fileScanner.nextLine();
-                String[] accountInfoArray = accountInfo.split(",");
-                if (Long.parseLong(accountInfoArray[0]) == accountNumber) {
-                    pw.println(accountInfoArray[0] + "," + accountInfoArray[1] + "," + accountInfoArray[2] + "," + newBalance);
-                } else {
-                    pw.println(accountInfoArray[0] + "," + accountInfoArray[1] + "," + accountInfoArray[2] + "," + accountInfoArray[3]);
-                }
-            }
-            fileScanner.close();
-            pw.flush();
-            pw.close();
-            accountData.delete();
-            File dump = new File("src/main/resources/data/account-data.csv");
-            tempFile.renameTo(dump);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public void repayLoan(Customer customer, Account account, Loan loan, double amount) {
         withdraw(account, amount);
         loan.repayLoan(amount);
         if (loan.getOutstandingAmount() > 0) {
-            csvEditLoanOutstandingAmount(loan.getLoanId(), loan.getOutstandingAmount());
+            CsvManager.csvEditLoanOutstandingAmount(loan.getLoanId(), loan.getOutstandingAmount());
         } else if (loan.getOutstandingAmount() == 0) {
             customer.getLoanTypeHashMap().remove(loan.loanType);
             loanDataHashMap.remove(loan.getLoanId());
-            csvDeleteLoan(loan.getLoanId());
+            CsvManager.csvDeleteLoan(loan.getLoanId());
         } else {
             deposit(account, -loan.getOutstandingAmount());
             customer.getLoanTypeHashMap().remove(loan.loanType);
             loanDataHashMap.remove(loan.getLoanId());
-            csvDeleteLoan(loan.getLoanId());
+            CsvManager.csvDeleteLoan(loan.getLoanId());
         }
 
-    }
-
-    private void csvDeleteLoan(long loanId) {
-        String tempFilePath = "src/main/resources/data/temporary-file.csv";
-        File accountData = new File("src/main/resources/data/loan-data.csv");
-        File tempFile = new File(tempFilePath);
-        try {
-            FileWriter fw = new FileWriter(tempFilePath, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            Scanner fileScanner = new Scanner(new File("src/main/resources/data/loan-data.csv"));
-            if (fileScanner.hasNextLine()) {
-                String headerLine = fileScanner.nextLine();
-                pw.println(headerLine);
-            }
-            while(fileScanner.hasNext()) {
-                String loanInfo = fileScanner.nextLine();
-                String[] loanInfoArray = loanInfo.split(",");
-                if (Long.parseLong(loanInfoArray[0]) != loanId) {
-                    pw.println(loanInfoArray[0] + "," + loanInfoArray[1] + "," + loanInfoArray[2] + "," + loanInfoArray[3]
-                            + "," + loanInfoArray[4] + "," + loanInfoArray[5] + "," + loanInfoArray[6]);
-                }
-            }
-            fileScanner.close();
-            pw.flush();
-            pw.close();
-            accountData.delete();
-            File dump = new File("src/main/resources/data/loan-data.csv");
-            tempFile.renameTo(dump);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void csvEditLoanOutstandingAmount(long loanId, double newOutstandingAmount) {
-        String tempFilePath = "src/main/resources/data/temporary-file.csv";
-        File accountData = new File("src/main/resources/data/loan-data.csv");
-        File tempFile = new File(tempFilePath);
-        try {
-            FileWriter fw = new FileWriter(tempFilePath, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            Scanner fileScanner = new Scanner(new File("src/main/resources/data/loan-data.csv"));
-            if (fileScanner.hasNextLine()) {
-                String headerLine = fileScanner.nextLine();
-                pw.println(headerLine);
-            }
-            while(fileScanner.hasNext()) {
-                String loanInfo = fileScanner.nextLine();
-                String[] loanInfoArray = loanInfo.split(",");
-                if (Long.parseLong(loanInfoArray[0]) == loanId) {
-                    pw.println(loanInfoArray[0] + "," + loanInfoArray[1] + "," + loanInfoArray[2] + "," + loanInfoArray[3]
-                            + "," + newOutstandingAmount + "," + loanInfoArray[5] + "," + loanInfoArray[6]);
-                } else {
-                    pw.println(loanInfoArray[0] + "," + loanInfoArray[1] + "," + loanInfoArray[2] + "," + loanInfoArray[3]
-                            + "," + loanInfoArray[4] + "," + loanInfoArray[5] + "," + loanInfoArray[6]);
-                }
-            }
-            fileScanner.close();
-            pw.flush();
-            pw.close();
-            accountData.delete();
-            File dump = new File("src/main/resources/data/loan-data.csv");
-            tempFile.renameTo(dump);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public HashMap<String, Customer> getCustomerDataHashMap() {
